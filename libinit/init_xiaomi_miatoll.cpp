5c81162d21beb068f7674ee5d133dbcf9329ce1f
Alexander Winkowski <dereference23@outlook.com>
2023-08-09 13:11:25 +0000

    miatoll: Update blobs
    Change-Id: I3dc32f921719609aae2a7ed1a38bb8d2a1cb6343

--- libinit/init_xiaomi_miatoll.cpp
+++ libinit/init_xiaomi_miatoll.cpp
@@ -15,7 +15,7 @@ static const variant_info_t curtanain_info = {
     .brand = "Redmi",
     .device = "curtana",
     .model = "Redmi Note 10 Lite",
-    .build_fingerprint = "Redmi/curtana_in1/curtana:11/RKQ1.200826.002/V12.5.10.0.RJWINRF:user/release-keys",
+    .build_fingerprint = "Redmi/curtana_in1/curtana:12/RKQ1.211019.001/V14.0.5.0.SJWINRF:user/release-keys",
 
     .nfc = false,
 };
@@ -26,7 +26,7 @@ static const variant_info_t curtana_info = {
     .brand = "Redmi",
     .device = "curtana",
     .model = "Redmi Note 9S",
-    .build_fingerprint = "Redmi/curtana_global/curtana:12/RKQ1.211019.001/V13.0.2.0.SJWMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/curtana_global/curtana:12/RKQ1.211019.001/V14.0.4.0.SJWMIXM:user/release-keys",
 
     .nfc = false,
 };
@@ -37,7 +37,7 @@ static const variant_info_t excalibur_info = {
     .brand = "Redmi",
     .device = "excalibur",
     .model = "Redmi Note 9 Pro Max",
-    .build_fingerprint = "Redmi/excalibur_in/excalibur:11/RKQ1.200826.002/V12.5.6.0.RJXINXM:user/release-keys",
+    .build_fingerprint = "Redmi/excalibur_in/excalibur:12/RKQ1.211019.001/V14.0.2.0.SJXINXM:user/release-keys",
 
     .nfc = false,
 };
@@ -48,7 +48,7 @@ static const variant_info_t gram_info = {
     .brand = "POCO",
     .device = "gram",
     .model = "POCO M2 Pro",
-    .build_fingerprint = "POCO/gram_in/gram:11/RKQ1.200826.002/V12.5.6.0.RJPINXM:user/release-keys",
+    .build_fingerprint = "POCO/gram_in/gram:12/RKQ1.211019.001/V14.0.5.0.SJPINXM:user/release-keys",
 
     .nfc = false,
 };
@@ -59,7 +59,7 @@ static const variant_info_t joyeuse_info = {
     .brand = "Redmi",
     .device = "joyeuse",
     .model = "Redmi Note 9 Pro",
-    .build_fingerprint = "Redmi/joyeuse_global/joyeuse:12/RKQ1.211019.001/V13.0.1.0.SJZMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/joyeuse_global/joyeuse:12/RKQ1.211019.001/V14.0.3.0.SJZMIXM:user/release-keys",
 
     .nfc = true,
 };
